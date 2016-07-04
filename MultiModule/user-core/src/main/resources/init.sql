2517f75d555f2551734a1347a1370115722361e0
xinyu jin <xinyu jin>
2016-07-05 01:30:54 +0800

    update

--- MultiModule/user-core/src/main/resources/init.sql
+++ MultiModule/user-core/src/main/resources/init.sql
@@ -1,4 +1,8 @@
-use itat_maven_test;
+CREATE DATABASE 'maven' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'jxy'@'%' IDENTIFIED BY '123456';#后面的表示密码ssm
+GRANT ALL PRIVILEGES ON maven.* TO 'jxy'@'%';
+FLUSH PRIVILEGES;
+use maven;
 create table IF NOT EXISTS t_user(
 	id int(10) auto_increment primary key,
 	username varchar(20),
